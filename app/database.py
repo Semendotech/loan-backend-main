e0a058db24cdebda5cefefa5f578c9be45ad2c17
Repo Sync <noreply@example.com>
2026-06-21 20:33:55 +0300

    fix: SSL connect_args for aiomysql

--- app/database.py
+++ app/database.py
@@ -13,7 +13,7 @@ engine = create_async_engine(
     DATABASE_URL,
     echo=True,
     future=True,
-    connect_args={"ssl": True},
+    connect_args={"ssl": {"check_hostname": False, "verify_mode": 0}},
 )
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
