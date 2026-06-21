c99484058e7484d2442dab1a047f513d1c5bbde4
Repo Sync <noreply@example.com>
2026-06-21 20:47:23 +0300

    feat: use SSL context for aiomysql connection

--- app/database.py
+++ app/database.py
@@ -9,11 +9,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 Base = declarative_base()
 
+import ssl as ssl_module
+
+ssl_ctx = ssl_module.create_default_context()
+ssl_ctx.check_hostname = False
+ssl_ctx.verify_mode = ssl_module.CERT_NONE
+
 engine = create_async_engine(
     DATABASE_URL,
     echo=True,
     future=True,
-    connect_args={"ssl": True},
+    connect_args={"ssl": ssl_ctx},
 )
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
